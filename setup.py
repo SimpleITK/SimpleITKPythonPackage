5cfff08ba7790972928cd2c1dcba1d0e201797b1
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-08-31 02:33:49 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from skbuild import setup
+
+setup(
+    name='SimpleITK',
+    version='0.10.0',
+    author='Insight Software Consortium',
+    author_email='insight-users@itk.org',
+    packages=['SimpleITK'],
+    package_dir={'SimpleITK':'SimpleITK'},
+    download_url=r'https://www.itk.org/SimpleITKDoxygen/html/PyDownloadPage.html',
+    description=r'Simplified interface to the Insight Toolkit for image registration and segmentation',
+    long_description='Provide an abstraction layer to ITK that enables developers\
+    and users to access the powerful features of the InsightToolkit in a more \
+    simplified manner.',
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering"
+        ],
+    license='Apache',
+    keywords='ITK InsightToolkit segmentation registration image',
+    url=r'http://simpleitk.org/',
+    install_requires=[
+        'numpy'
+    ]
+    )
