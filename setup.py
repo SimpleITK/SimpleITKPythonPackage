0caa9ac28e2d44ba5d619c9fc4d0e6e2077edd30
Bradley Lowekamp <blowekamp@mail.nih.gov>
2017-02-09 08:55:44 -0500

    Correct package version for PEP 440 compliance
    The invalid `-` deliminator was used in stead of `+` for a local
    version identifier. However for the `dev` component just the `.`
    deliminator is needed.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from skbuild import setup
 
 setup(
     name='SimpleITK',
-    version='0.11.0-dev',
+    version='0.11.0.dev',
     author='Insight Software Consortium',
     author_email='insight-users@itk.org',
     packages=['SimpleITK'],
