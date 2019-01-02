264138ebe22122a76c02e93dc38f17c0eef3ef23
Bradley Lowekamp <blowekamp@mail.nih.gov>
2019-01-02 15:45:30 -0500

    Update to SimpleITK v1.2.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 setup(
     name='SimpleITK',
-    version='1.2.0rc1',
+    version='1.2.0',
     author='Insight Software Consortium',
     author_email='insight-users@itk.org',
     packages=['SimpleITK'],
