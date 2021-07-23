be3c146068d09c423084c91aa2cf54dc990407f9
Bradley Lowekamp <blowekamp@mail.nih.gov>
2021-07-23 15:03:40 +0000

    Update setup.py meta-data to match from main SimpleITK repo

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from __future__ import print_function
 from os import sys
 from skbuild import setup
 
+with open('SimpleITK/Readme.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name='SimpleITK',
@@ -12,9 +15,8 @@ setup(
     package_dir={'SimpleITK':'SimpleITK'},
     download_url=r'https://www.itk.org/SimpleITKDoxygen/html/PyDownloadPage.html',
     description=r'SimpleITK is a simplified interface to the Insight Toolkit (ITK) for image registration and segmentation',
-    long_description =r'SimpleITK provides an abstraction layer to ITK that enables developers \
-                        and users to access the powerful features of the InsightToolkit in an easy \
-                        to use manner for biomedical image analysis.',
+    long_description = long_description,
+    long_description_content_type='text/markdown',
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
@@ -33,8 +35,13 @@ setup(
         "Operating System :: MacOS"
         ],
     license='Apache',
-    keywords='SimpleITK ITK InsightToolkit segmentation registration',
-    url=r'https://simpleitk.org/',
+    keywords = 'SimpleITK ITK InsightToolkit segmentation registration',
+    url = r'http://simpleitk.org/',
+    project_urls={
+        "Bug Tracker": "https://github.com/SimpleITK/SimpleITK/issues",
+        "Documentation": "https://simpleitk.readthedocs.io/en/release/",
+        "Source Code": "https://github.com/SimpleITK/SimpleITK",
+    },
     install_requires=[],
     zip_safe=False
     )
