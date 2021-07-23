f7e89ab516e06bcf24e78f14340db12f8d6e4ce0
Bradley Lowekamp <blowekamp@mail.nih.gov>
2021-07-23 12:46:51 +0000

    Update SimpleITK to v2.1.0 tag

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from skbuild import setup
 
 setup(
     name='SimpleITK',
-    version='2.0.0',
+    version='2.1.0',
     author='Insight Software Consortium',
     author_email='insight-users@itk.org',
     packages=['SimpleITK'],
